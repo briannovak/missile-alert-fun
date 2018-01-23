2676b1983e0732ee9cd064300f8e9a58c55587b4
Paul Highum <phigh07@gmail.com>
2018-01-23 15:01:46 -0700

    adds puzzle.js

---
+++ puzzle.js
@@ -0,0 +1,6 @@
+const puzzleForm = document.querySelector(".puzzle")
+puzzleForm.addEventListener("submit", event => {
+  event.preventDefault()
+  document.querySelector(".response").innerHTML =
+    "Huh? I... I don't know that. Auuuuuuuugh."
+})
